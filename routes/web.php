bea2d754c4830712720469998b30a3d77b2a1b25
Mohammad Bayu Rizki <widjanarko020@gmail.com>
2025-11-21 10:57:54 +0700

    fix: update PHP version to 8.2 and clean routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application.
+| These routes are loaded by the RouteServiceProvider and all of them
+| will be assigned to the "web" middleware group.
+|
+*/
 
 Route::get('/', function () {
     return response()->json([
         'message' => 'BbiHub API',
-        'version' => '1.0.0'
+        'version' => '1.0.0',
+        'status' => 'running'
     ]);
 });
